a04a9d4713b3c96c0725324a32609fda28b07399
Arjun Agrawal <38666062+YarkLink@users.noreply.github.com>
2018-04-23 11:13:23 -0700

    XD

--- src/Piece.java
+++ src/Piece.java
@@ -1,4 +1,22 @@
-
+/**
+ * 
+ * @author Arjun Agrawal
+ *
+ */
 public abstract class Piece {
+	private String name;
+	private int color;
 
+	public Piece(String name, int color) {
+		this.name = name;
+		this.color = color;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getColor() {
+		return color;
+	}
 }
---
+++ src/Player.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Player {
+	private ArrayList<Piece> pieces;
+	int color;
+
+	public Player(int color) {
+		this.color = color;
+		pieces = new ArrayList<Piece>(16);
+
+	}
+
+}
