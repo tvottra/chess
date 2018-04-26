1d9d209b6f0088460897617472225ffc15861886
jlim128 <38666100+jlim128@users.noreply.github.com>
2018-04-26 06:04:50 -0700

    Update Queen.java

--- src/Queen.java
+++ src/Queen.java
@@ -7,8 +7,53 @@ public class Queen extends Piece {
 	}
 
 	@Override
-	public boolean isLegal(Position move) {
-		// TODO Auto-generated method stub
+	public boolean isLegal(Position move, int[][] board) {
+		//check if move is off of board, if yes, return false
+		if((move.getCol() > 7 || move.getCol() < 0) && (move.getRow() > 7 || move.getRow() < 0)){
+			return false;
+		}
+		//check if move is in the same column or row, if yes,
+		//check if move is behind another piece, if yes, return false, else truw
+		if(move.getCol() == super.pos.getCol()){
+			int rowOfOther = -1;
+			if(move.getRow() > super.pos.getRow(){
+				for(int i = super.pos.getRow(); i < board.length; i++){
+					if(board[i][move.getCol()] != null){
+						rowOfOther = i;
+						break;
+					}
+				}
+				if((move.getRow()
+			}
+			else{
+				for(int i = super.pos.getRow(); i >= 0; i--){
+					if(board[i][move.getCol()] != null){
+						rowOfOther = i;
+						break;
+					}
+				}
+			}
+			return true;
+		}
+		if(move.getRow() == super.pos.getRow()){
+			
+			return true;
+		}
+		//check if move is diagonal to the queen; if yes, return true
+		for(int i = 1; i < board.length; i++){
+			if(move.getRow() == super.pos.getRow() + i && move.getCol() == super.pos.getCol() + i){
+				return true;
+			}
+			if(move.getRow() == super.pos.getRow() - i && move.getCol() == super.pos.getCol() + i){
+				return true;
+			}
+			if(move.getRow() == super.pos.getRow() + i && move.getCol() == super.pos.getCol() - i){
+				return true;
+			}
+			if(move.getRow() == super.pos.getRow() - i && move.getCol() == super.pos.getCol() - i){
+				return true;
+			}
+		}
 		return false;
 	}
 
