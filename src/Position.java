679aa25e52b6346d65aa721d0dff230f928a774f
Arjun Agrawal <38666062+YarkLink@users.noreply.github.com>
2018-04-26 19:40:20 -0700

    Added .equals method

--- src/Position.java
+++ src/Position.java
@@ -1,28 +1,33 @@
 
 public class Position {
 
-	int row;
-	int column;
+    int row;
+    int column;
 
-	public Position(int row, int column) {
-		this.row = row;
-		this.column = column;
-	}
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
 
-	public int getRow() {
-		return row;
-	}
+    public int getRow() {
+        return row;
+    }
 
-	public int getColumn() {
-		return column;
-	}
+    public int getColumn() {
+        return column;
+    }
 
-	public void setRow(int row) {
-		this.row = row;
-	}
+    public void setRow(int row) {
+        this.row = row;
+    }
 
-	public void setColumn(int column) {
-		this.column = column;
-	}
+    public void setColumn(int column) {
+        this.column = column;
+    }
 
+    public boolean equals(Position other) {
+        if (this.row == other.getRow() && this.column == other.getRow())
+            return true;
+        return false;
+    }
 }
