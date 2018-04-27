12badd012803bc652a0a6c0a8584237a81b44f23
leandrew04 <38666101+leandrew04@users.noreply.github.com>
2018-04-26 17:37:38 -0700

    Add files via upload
    Test commit for Rook class

---
+++ Rook.java
@@ -0,0 +1,20 @@
+
+public class Rook extends Piece {
+	private boolean hasAlreadyMoved;	
+	/**
+	 * Constructor to initialize fields
+	 * 
+	 */
+	public Rook(int color, Position pos) {
+		super(color, pos, 5);
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean isLegal(Position move) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
