5f686a4cb0cc1f6f6deee92a13f7446aeadfaebb
leandrew04 <38666101+leandrew04@users.noreply.github.com>
2018-04-27 17:21:48 -0700

    Update Board.java
    Updated according to class hierarchy spreadsheet

--- src/Board.java
+++ src/Board.java
@@ -1,14 +1,25 @@
-
+/**
+ * Class that represents the chess board
+ * 
+ * @author Tommy Tran
+ * @author Arjun Agrawal
+ * @author Andrew Le
+ *
+ */
 public class Board {
-	Piece[][] board;
-	private final int NUM_PAWN = 8;
+	Tile[][] board;
+	private final int SIZE = 8;
 
+	/**
+	 * Default constructor that sets up the chess board with all pieces in their
+	 * starting positions
+	 */
 	public Board() {
-		board = new Slot[8][8];
+		board = new Tile[8][8];
 		// generates white side first, then black
 		for (int pos = 7, color = 0; color < 2; color++, pos -= 6) {
 			// generate color pawns pawns
-			for (int x = 0; x < NUM_PAWN; x++) {
+			for (int x = 0; x < SIZE; x++) {
 				int temppos = pos - 1;
 				if (temppos < 0) {
 					temppos = 0 - temppos;
@@ -26,10 +37,50 @@ public class Board {
 		}
 	}
 
-	public Piece[][] getBoard() {
+	/**
+	 * Accessor method to get this Board
+	 * 
+	 * @return the Board
+	 */
+	public Tile[][] getBoard() {
 		return board;
 	}
 
+	/**
+	 * Accessor method to get a tile at the specified location
+	 * 
+	 * @param row
+	 *            - the row index of the Tile
+	 * @param col
+	 *            - the column index of the Tile
+	 * @return the Tile at board[row][col]
+	 */
+	public Tile getTile(int row, int col) {
+		return board[row][col];
+	}
+
+	/**
+	 * Called each time after a Piece is moved, looping through all of the Tiles and
+	 * updating the white and black control for each Tile
+	 */
+	public void updateControl() {
+		// TO BE IMPLEMENTED
+	}
+	
+	/**
+	 * Attempts to move a Piece on a Tile to the given Position
+	 * @param fromPos - the Piece's current position
+	 * @param toPos - the Position to which the Piece will be moved
+	 */
+	public void moveMyPiece(Position fromPos, Position toPos) {
+		// TO BE IMPLEMENTED
+	}
+
+	/**
+	 * toString method
+	 * 
+	 * @return
+	 */
 	public String toString() {
 		String x = "";
 		for (int i = 0; i < board.length; i++) {
