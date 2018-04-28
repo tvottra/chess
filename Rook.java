7339bd3f52c424c489e68e76eeb52003e869f762
leandrew04 <38666101+leandrew04@users.noreply.github.com>
2018-04-27 20:15:37 -0700

    Completed Rook.java

--- Rook.java
+++ Rook.java
@@ -1,20 +1,88 @@
 
+/**
+ * Class that represents a Rook chess piece
+ */
+import java.util.ArrayList;
+
+/**
+ * @author Andrew Le
+ *
+ */
 public class Rook extends Piece {
-	private boolean hasAlreadyMoved;	
+	private ArrayList<Position> fieldOfControl;
+	private boolean hasMoved;
+
 	/**
-	 * Constructor to initialize fields
-	 * 
+	 * @param name
+	 *            - the name of the piece
+	 * @param color
+	 *            - the color of the piece
+	 * @param pos
+	 *            - the Position of the piece
+	 * @param pointVal
+	 *            - the point value of the Piece
 	 */
-	public Rook(int color, Position pos) {
-		super(color, pos, 5);
-		
-		// TODO Auto-generated constructor stub
+	public Rook(String name, int color, Position pos, int pointVal) {
+		super(name, color, pos, pointVal);
+		hasMoved = false;
+	}
+
+	/**
+	 * Accessor method to get this Rook's move status
+	 * 
+	 * @return true if this Rook has already moved at least once, false otherwise
+	 */
+	public boolean hasMoved() {
+		return hasMoved;
+	}
+
+	/**
+	 * Mutator method to update the Rook's move state
+	 * 
+	 * @param moveState
+	 *            - true if this Rook has already moved at least once, false
+	 *            otherwise
+	 */
+	public void setMoveState(boolean moveState) {
+		hasMoved = moveState;
 	}
 
 	@Override
-	public boolean isLegal(Position move) {
-		// TODO Auto-generated method stub
-		return false;
+	public void setFieldOfControl() {
+		Position pos = getPosition();
+		fieldOfControl = null;
+		fieldOfControl = new ArrayList<Position>();
+		// Add all positions above current position (same column)
+		for (int r = 0; r < pos.getRow(); r++) {
+			Position p = new Position(r, pos.getColumn());
+			fieldOfControl.add(p);
+		}
+		// Add all positions below current position (same column)
+		for (int r = pos.getRow() + 1; r < 8; r++) {
+			Position p = new Position(r, pos.getColumn());
+			fieldOfControl.add(p);
+		}
+		// Add all positions left of current position (same row)
+		for (int c = 0; c < pos.getColumn(); c++) {
+			Position p = new Position(pos.getRow(), c);
+			fieldOfControl.add(p);
+		}
+		// Add all positions left of current position (same row)
+		for (int c = pos.getColumn() + 1; c < 8; c++) {
+			Position p = new Position(pos.getRow(), c);
+			fieldOfControl.add(p);
+		}
+
 	}
 
+	@Override
+	public boolean isWithinRangeOfMovement(Position toPos) {
+		setFieldOfControl();
+		for (Position pos : fieldOfControl) {
+			if (pos.equals(toPos)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
