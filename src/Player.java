834274c265a514ad54c571dcb2dc4fc24352d103
tvottra <tvottra@gmail.com>
2018-05-07 10:24:27 -0700

    Made extra sure reverting to Git commit of 'Merge pull request #32 from tvottra/arjun' by copy pasting all the files in that revision

--- src/Player.java
+++ src/Player.java
@@ -4,128 +4,128 @@
  * @author Andrew Le
  */
 public class Player {
-	private String myName;
-	private int myNumber;
-	private boolean checkmated;
-	private boolean resigned;
+    private String myName;
+    private int myNumber;
+    private boolean checkmated;
+    private boolean resigned;
 
-	/**
-	 * Constructor to initialize this player's name and number
-	 *
-	 * @param name   - the given name
-	 * @param number - the given number
-	 */
-	public Player(String name, int number) {
-		myName = name;
-		myNumber = number;
-		checkmated = false;
-		resigned = false;
-	}
+    /**
+     * Constructor to initialize this player's name and number
+     *
+     * @param name   - the given name
+     * @param number - the given number
+     */
+    public Player(String name, int number) {
+        myName = name;
+        myNumber = number;
+        checkmated = false;
+        resigned = false;
+    }
 
-	/**
-	 * Accessor method to get the player's name
-	 *
-	 * @return the player's name
-	 */
-	public String getName() {
-		return myName;
-	}
+    /**
+     * Accessor method to get the player's name
+     *
+     * @return the player's name
+     */
+    public String getName() {
+        return myName;
+    }
 
-	/**
-	 * Mutator method to change the player's name
-	 *
-	 * @param name - the new name
-	 */
-	public void setName(String name) {
-		myName = name;
-	}
+    /**
+     * Mutator method to change the player's name
+     *
+     * @param name - the new name
+     */
+    public void setName(String name) {
+        myName = name;
+    }
 
-	/**
-	 * Accessor method to get the player's number
-	 *
-	 * @return the player's number
-	 */
-	public int getNumber() {
-		return myNumber;
-	}
+    /**
+     * Accessor method to get the player's number
+     *
+     * @return the player's number
+     */
+    public int getNumber() {
+        return myNumber;
+    }
 
-	/**
-	 * Accessor method to get the checkmate status
-	 *
-	 * @return true if this player is checkmated, false otherwise
-	 */
-	public boolean isCheckMated() {
-		return checkmated;
-	}
+    /**
+     * Accessor method to get the checkmate status
+     *
+     * @return true if this player is checkmated, false otherwise
+     */
+    public boolean isCheckMated() {
+        return checkmated;
+    }
 
-	/**
-	 * Mutator method to set the checkmate status
-	 *
-	 * @param checkmate - true if the player has been checkmated, false otherwise
-	 */
-	public void setCheckMate(boolean checkmate) {
-		checkmated = checkmate;
-	}
+    /**
+     * Mutator method to set the checkmate status
+     *
+     * @param checkmate - true if the player has been checkmated, false otherwise
+     */
+    public void setCheckMate(boolean checkmate) {
+        checkmated = checkmate;
+    }
 
-	/**
-	 * Mutator method to set the resign status
-	 *
-	 * @param resign - true if the player has resigned, false otherwise
-	 */
-	public void setResign(boolean resign) {
-		resigned = resign;
-	}
+    /**
+     * Mutator method to set the resign status
+     *
+     * @param resign - true if the player has resigned, false otherwise
+     */
+    public void setResign(boolean resign) {
+        resigned = resign;
+    }
 
-	/**
-	 * Accessor method to get the resign status
-	 *
-	 * @return true if the player has resigned, false otherwise
-	 */
-	public boolean isResigned() {
-		return resigned;
-	}
+    /**
+     * Accessor method to get the resign status
+     *
+     * @return true if the player has resigned, false otherwise
+     */
+    public boolean isResigned() {
+        return resigned;
+    }
 
-	/**
-	 * Determines hether this player and the given player are the same player
-	 *
-	 * @param other - the given player
-	 * @return true if both players are the same player, false otherwise
-	 */
-	public boolean equals(Player other) {
-		if (myName.equals(other.getName()) && myNumber == other.getNumber()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    /**
+     * Determines hether this player and the given player are the same player
+     *
+     * @param other - the given player
+     * @return true if both players are the same player, false otherwise
+     */
+    public boolean equals(Player other) {
+        if (myName.equals(other.getName()) && myNumber == other.getNumber()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	/**
-	 * Requests a draw from the opposing player; calls acceptDraw
-	 *
-	 * @return true if this player offers a draw; false otherwise
-	 */
-	public boolean requestDraw(Player other) {
-		// TO BE IMPLEMENTED
-		return true;
-	}
+    /**
+     * Requests a draw from the opposing player; calls acceptDraw
+     *
+     * @return true if this player offers a draw; false otherwise
+     */
+    public boolean requestDraw(Player other) {
+        // TO BE IMPLEMENTED
+        return true;
+    }
 
-	/**
-	 * Responds to a draw offer from the opposing player
-	 *
-	 * @param other - the opposing player
-	 * @return true if this player accepts the draw offer, false otherwise
-	 */
-	public boolean acceptDraw(Player other) {
-		// TO BE IMPLEMENTED
-		return true;
-	}
+    /**
+     * Responds to a draw offer from the opposing player
+     *
+     * @param other - the opposing player
+     * @return true if this player accepts the draw offer, false otherwise
+     */
+    public boolean acceptDraw(Player other) {
+        // TO BE IMPLEMENTED
+        return true;
+    }
 
-	/**
-	 * toString method to print this player's information
-	 */
-	public String toString() {
-		return myName + "(" + myNumber + ")";
-	}
+    /**
+     * toString method to print this player's information
+     */
+    public String toString() {
+        return myName + "(" + myNumber + ")";
+    }
 
 
 }
