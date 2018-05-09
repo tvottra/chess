8773178107f42b1a94281e1b00911e88a7409cc9
leandrew04 <38666101+leandrew04@users.noreply.github.com>
2018-05-09 16:29:05 -0700

    Has methods to setup individual pieces

--- src/GameTester.java
+++ src/GameTester.java
@@ -16,8 +16,8 @@ public class GameTester {
 		Game test = new Game(name1, name2);
 		Board myBoard = test.getBoard();
 		clearBoard(myBoard);
-		generatePawns(0,myBoard);
-		setupKnight(myBoard);
+		generatePawns(1, myBoard);
+		setupQueens(myBoard);
 		System.out.println(myBoard);
 		test.playGame();
 	}
@@ -31,17 +31,53 @@ public class GameTester {
 	}
 
 	/**
-	 * Determines the Position and color for a select number of Knights on the given
-	 * board
+	 * Determines the Position and color for the 4 Rooks on the given board
 	 * 
 	 * @param aBoard
 	 *            - the given board
 	 */
-	public static void setupKnight(Board aBoard) {
+	public static void setupRooks(Board aBoard) {
+		Position whitePos = new Position(7, 0);
+		Position whitePos2 = new Position(7, 7);
+		Position blackPos = new Position(0, 0);
+		Position blackPos2 = new Position(0, 7);
+		createRooks(whitePos, 0, aBoard);
+		createRooks(whitePos2, 0, aBoard);
+		createRooks(blackPos, 1, aBoard);
+		createRooks(blackPos2, 1, aBoard);
+	}
+
+	/**
+	 * 
+	 * Creates a Rook Piece on the given board with the given color and Position
+	 * 
+	 * @param pos
+	 *            - the rook's Position [row][col]
+	 * @param color
+	 *            - the rook's color; 0 = white, 1 = black
+	 * @param aBoard
+	 *            - the given board
+	 */
+	public static void createRooks(Position pos, int color, Board aBoard) {
+		Piece Rook = new Rook(color, pos);
+		aBoard.getTile(pos).setPiece(Rook);
+	}
+
+	/**
+	 * Determines the Position and color for the 4 Knights on the given board
+	 * 
+	 * @param aBoard
+	 *            - the given board
+	 */
+	public static void setupKnights(Board aBoard) {
 		Position whitePos = new Position(7, 1);
+		Position whitePos2 = new Position(7, 6);
 		Position blackPos = new Position(0, 1);
-		createKnight(whitePos, 0, aBoard);
-		createKnight(blackPos, 1, aBoard);
+		Position blackPos2 = new Position(0, 6);
+		createKnights(whitePos, 0, aBoard);
+		createKnights(whitePos2, 0, aBoard);
+		createKnights(blackPos, 1, aBoard);
+		createKnights(blackPos2, 1, aBoard);
 	}
 
 	/**
@@ -55,9 +91,100 @@ public class GameTester {
 	 * @param aBoard
 	 *            - the given board
 	 */
-	public static void createKnight(Position pos, int color, Board aBoard) {
+	public static void createKnights(Position pos, int color, Board aBoard) {
 		Piece knight = new Knight(color, pos);
 		aBoard.getTile(pos).setPiece(knight);
+	}
+
+	/**
+	 * Determines the Position and color for the 4 Bishops on the given board
+	 * 
+	 * @param aBoard
+	 *            - the given board
+	 */
+	public static void setupBishops(Board aBoard) {
+		Position whitePos = new Position(7, 2);
+		Position whitePos2 = new Position(7, 5);
+		Position blackPos = new Position(0, 2);
+		Position blackPos2 = new Position(0, 5);
+		createBishops(whitePos, 0, aBoard);
+		createBishops(whitePos2, 0, aBoard);
+		createBishops(blackPos, 1, aBoard);
+		createBishops(blackPos2, 1, aBoard);
+	}
+
+	/**
+	 * 
+	 * Creates a Bishop Piece on the given board with the given color and Position
+	 * 
+	 * @param pos
+	 *            - the bishop's Position [row][col]
+	 * @param color
+	 *            - the bishop's color; 0 = white, 1 = black
+	 * @param aBoard
+	 *            - the given board
+	 */
+	public static void createBishops(Position pos, int color, Board aBoard) {
+		Piece bishop = new Bishop(color, pos);
+		aBoard.getTile(pos).setPiece(bishop);
+	}
+
+	/**
+	 * Determines the Position and color for the 2 Queens on the given board
+	 * 
+	 * @param aBoard
+	 *            - the given board
+	 */
+	public static void setupQueens(Board aBoard) {
+		Position whitePos = new Position(7, 4);
+		Position blackPos = new Position(0, 4);
+		createQueens(whitePos, 0, aBoard);
+		createQueens(blackPos, 1, aBoard);
+	}
+
+	/**
+	 * 
+	 * Creates a Queen Piece on the given board with the given color and Position
+	 * 
+	 * @param pos
+	 *            - the queen's Position [row][col]
+	 * @param color
+	 *            - the queen's color; 0 = white, 1 = black
+	 * @param aBoard
+	 *            - the given board
+	 */
+	public static void createQueens(Position pos, int color, Board aBoard) {
+		Piece queen = new Queen(color, pos);
+		aBoard.getTile(pos).setPiece(queen);
+	}
+
+	/**
+	 * Determines the Position and color for the 2 Kings on the given board
+	 * 
+	 * @param aBoard
+	 *            - the given board
+	 */
+	public static void setupKings(Board aBoard) {
+		Position whitePos = new Position(7, 5);
+		Position blackPos = new Position(0, 5);
+		createKings(whitePos, 0, aBoard);
+		createKings(blackPos, 1, aBoard);
+	}
+
+	/**
+	 * 
+	 * Creates a King Piece on the given board with the given color and Position
+	 * 
+	 * @param pos
+	 *            - the king's Position [row][col]
+	 * @param color
+	 *            - the king's color; 0 = white, 1 = black
+	 * @param aBoard
+	 *            - the given board
+	 */
+	public static void createKings(Position pos, int color, Board aBoard) {
+		Piece king = new King(color, pos);
+		aBoard.getTile(pos).setPiece(king);
 	}
 
 	/**
