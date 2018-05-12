17196f5a82ee3e80a6579aa33cd51f498a1ac64b
leandrew04 <38666101+leandrew04@users.noreply.github.com>
2018-05-12 16:14:29 -0700

    Update Queen.java

--- src/Queen.java
+++ src/Queen.java
@@ -11,71 +11,12 @@ public class Queen extends Piece {
 	 * Constructor to initialize the Queen
 	 *
 	 * @param color
-	 *            - the Queen's color
+	 *            the Queen's color
 	 * @param pos
-	 *            - the Queen's Position
+	 *            the Queen's Position
 	 */
 	public Queen(int color, Position pos) {
 		super("Queen", color, pos, 9);
-	}
-
-	@Override
-	/**
-	 * Returns an array of the Positions that would be crossed if this Queen were to
-	 * move to the given Position
-	 *
-	 * @param toPos
-	 *            - the given Position
-	 * @return an ArrayList of the Positions that would be crossed
-	 */
-	public ArrayList<Position> getCrossedPositions(Position toPos) {
-		ArrayList<Position> positions = new ArrayList<Position>();
-		Position fromPos = getPosition();
-		// The two Positions are in the same row
-		if (fromPos.getRow() == toPos.getRow()) {
-			int i;
-			if (fromPos.isLeftOf(toPos)) {
-				i = 1;
-			} else {
-				i = -1;
-			}
-			for (int c = fromPos.getColumn(); c != toPos.getColumn(); c += i) {
-				Position pos = new Position(fromPos.getRow(), c);
-				positions.add(pos);
-			}
-		}
-		// The two Positions are in the same column
-		if (fromPos.getColumn() == toPos.getColumn()) {
-			int i;
-			if (fromPos.isAbove(toPos)) {
-				i = 1;
-			} else {
-				i = -1;
-			}
-			for (int r = fromPos.getRow(); r != toPos.getRow(); r += i) {
-				Position pos = new Position(r, fromPos.getColumn());
-				positions.add(pos);
-			}
-		}
-		if (getPosition().hasAbsSlopeOfOne(toPos)) {
-			int r, c;
-			if (fromPos.isAbove(toPos)) {
-				r = 1;
-			} else {
-				r = -1;
-			}
-			if (fromPos.isLeftOf(toPos)) {
-				c = 1;
-			} else {
-				c = -1;
-			}
-			while (!fromPos.equals(toPos)) {
-				positions.add(new Position(fromPos));
-				fromPos.addToRow(r);
-				fromPos.addToColumn(c);
-			}
-		}
-		return positions;
 	}
 
 	@Override
@@ -147,18 +88,5 @@ public class Queen extends Piece {
 			currentPos.addToColumn(1);
 		}
 		return rom;
-	}
-
-	@Override
-	/**
-	 * Checks whether the given Position is within this Queen's range of movement
-	 *
-	 * @param toPos
-	 *            - the destination Position
-	 * @return true if toPos is within this Queen's range of movement, false
-	 *         otherwise
-	 */
-	public boolean isWithinRangeOfMovement(Position toPos) {
-		return getRangeOfMovement().contains(toPos);
 	}
 }
