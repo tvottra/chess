70ecc206fe47085e79653625a23fcee37a0c1957
leandrew04 <38666101+leandrew04@users.noreply.github.com>
2018-05-12 16:12:50 -0700

    Update Pawn.java

--- src/Pawn.java
+++ src/Pawn.java
@@ -12,38 +12,13 @@ public class Pawn extends Piece {
 	 * Contructor to initialize the Pawn's color and Position
 	 *
 	 * @param color
-	 *            - the Pawn's color
+	 *            the Pawn's color
 	 * @param pos
-	 *            - the Pawn's Position
+	 *            the Pawn's Position
 	 */
 	public Pawn(int color, Position pos) {
 		super("Pawn", color, pos, 1);
 		hasMovedTwo = false;
-	}
-
-	@Override
-	/**
-	 * Returns an array of the Positions that would be crossed if this Pawn were to
-	 * move to the given Position
-	 *
-	 * @param toPos
-	 *            - the given Position
-	 * @return an ArrayList of the Positions that would be crossed
-	 */
-	public ArrayList<Position> getCrossedPositions(Position toPos) {
-		ArrayList<Position> iWillCross = new ArrayList<Position>();
-		if (isWithinRangeOfMovement(toPos)) {
-			iWillCross.add(getPosition());
-			if (Math.abs(toPos.getRow() - getPosition().getRow()) == 2) {
-				if (getColor() == 1) {
-					iWillCross.add(new Position(getPosition().getRow() + 1, getPosition().getColumn()));
-				} else {
-					iWillCross.add(new Position(getPosition().getRow() - 1, getPosition().getColumn()));
-				}
-			}
-			iWillCross.add(toPos);
-		}
-		return iWillCross;
 	}
 
 	@Override
@@ -56,7 +31,6 @@ public class Pawn extends Piece {
 	public ArrayList<Position> getRangeOfMovement() {
 		ArrayList<Position> rom = new ArrayList<Position>();
 		Position currentPos = new Position(getPosition());
-
 		int r = 0;
 		if (getColor() == 0) {
 			r = -1;
@@ -65,7 +39,6 @@ public class Pawn extends Piece {
 		} else {
 			System.out.println("Incorrect implementation of Pawn ROM");
 		}
-
 		rom.add(new Position(currentPos.getRow() + r, currentPos.getColumn())); // Add Position 1 Tile in front of Piece
 
 		if (!hasMoved()) {
@@ -73,28 +46,6 @@ public class Pawn extends Piece {
 																						// of Piece
 		}
 		return rom;
-	}
-
-	@Override
-	/**
-	 * Checks whether the given Position is within this Pawn's range of movement
-	 *
-	 * @param toPos
-	 *            - the destination Position
-	 * @return true if toPos is within this Pawn's range of movement, false
-	 *         otherwise
-	 */
-	public boolean isWithinRangeOfMovement(Position toPos) {
-		int fromRow = getPosition().getRow();
-		int fromCol = getPosition().getColumn();
-		int toRow = toPos.getRow();
-		int toCol = toPos.getColumn();
-		if (!hasMoved()) {
-			return ((toPos.isWithinBounds()) && (fromCol == toCol)
-					&& (Math.abs(fromRow - toRow) == 2 || Math.abs(fromRow - toRow) == 1));
-		} else {
-			return ((toPos.isWithinBounds()) && (fromCol == toCol) && Math.abs(fromRow - toRow) == 1);
-		}
 	}
 
 	/**
@@ -123,10 +74,9 @@ public class Pawn extends Piece {
 	 * Mutator method to update the Pawn's 2-tile move status
 	 * 
 	 * @param moved
-	 *            - true if the Pawn has used the 2-tile move, false otherwise
+	 *            true if the Pawn has used the 2-tile move, false otherwise
 	 */
 	public void setHasMovedTwo(boolean moved) {
 		hasMovedTwo = moved;
 	}
-
 }
