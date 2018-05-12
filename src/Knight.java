09236a639a39de64ae17b6c09d9c892f4d5d581d
leandrew04 <38666101+leandrew04@users.noreply.github.com>
2018-05-12 16:12:27 -0700

    Update Knight.java

--- src/Knight.java
+++ src/Knight.java
@@ -11,9 +11,9 @@ public class Knight extends Piece {
 	 * Contructor to initialize the Pawn's color and position
 	 * 
 	 * @param color
-	 *            - the Knight's color
+	 *            the Knight's color
 	 * @param pos
-	 *            - the Knight's Position
+	 *            the Knight's Position
 	 */
 	public Knight(int color, Position pos) {
 		super("Knight", color, pos, 3);
@@ -21,27 +21,8 @@ public class Knight extends Piece {
 
 	@Override
 	/**
-	 * Returns an array of the Positions that would be crossed if this Knight were
-	 * to move to the given Position
-	 *
-	 * @param toPos
-	 *            - the given Position
-	 * @return an ArrayList of the Positions that would be crossed
-	 */
-	public ArrayList<Position> getCrossedPositions(Position toPos) {
-		if (isWithinRangeOfMovement(toPos)) {
-			ArrayList<Position> pos = new ArrayList<Position>();
-			pos.add(toPos);
-			return pos;
-		}
-		return null;
-	}
-
-	@Override
-	/**
 	 * Calculates the Knight's range of movement based on known board size and its
-	 * current position. Positions are ordered ascending in terms of row then
-	 * column. E.g., (0, 0), (0, 1), (0, 2), (1, 0)...
+	 * current position.
 	 *
 	 * @return the Knight's range of movement
 	 */
@@ -64,18 +45,5 @@ public class Knight extends Piece {
 		if (new Position(this.getPosition().getRow() - 1, this.getPosition().getColumn() - 2).isWithinBounds())
 			field.add(new Position(this.getPosition().getRow() - 1, this.getPosition().getColumn() - 2));
 		return field;
-	}
-
-	/**
-	 * Checks whether the given Position is within this Knight's range of movement
-	 *
-	 * @param move
-	 *            - the destination Position
-	 * @return true if toPos is within this Knight's range of movement, false
-	 *         otherwise
-	 */
-	@Override
-	public boolean isWithinRangeOfMovement(Position move) {
-		return getRangeOfMovement().contains(move);
 	}
 }
